71960404635d698209115a0d617bb3871c06b82d
Sendhil Grandhi <sendhilgrandhi@yahoo.co.in>
2019-04-20 14:14:05 -0400

    adding coroutines test rule

---
+++ app/src/test/java/com/intuit/sendhil/gitexplorer/util/CoroutineTestRule.kt
@@ -0,0 +1,19 @@
+package com.intuit.sendhil.gitexplorer.util
+
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.test.resetMain
+import kotlinx.coroutines.test.setMain
+import org.junit.rules.TestWatcher
+import org.junit.runner.Description
+
+class CoroutineTestRule: TestWatcher() {
+    override fun finished(description: Description?) {
+        super.finished(description)
+        Dispatchers.setMain(Dispatchers.Unconfined)
+    }
+
+    override fun starting(description: Description?) {
+        super.starting(description)
+        Dispatchers.resetMain()
+    }
+}
