9373a448e8d3c06cab28580be13af55987c65fa8
Sendhil Grandhi <sendhilgrandhi@yahoo.co.in>
2019-04-20 13:47:28 -0400

    LIST: wireup repository to ui adapter

--- app/src/main/java/com/intuit/sendhil/gitexplorer/ui/list/GitRepoListFragment.kt
+++ app/src/main/java/com/intuit/sendhil/gitexplorer/ui/list/GitRepoListFragment.kt
@@ -4,20 +4,29 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.view.ViewCompat
 import androidx.lifecycle.Observer
+import androidx.recyclerview.widget.DefaultItemAnimator
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.intuit.sendhil.gitexplorer.R
 import com.intuit.sendhil.gitexplorer.model.Repos
 import com.intuit.sendhil.gitexplorer.viewmodel.list.GitRepoListFeatureViewModel
 import dagger.android.support.DaggerFragment
+import kotlinx.android.synthetic.main.list_fragment.*
 import timber.log.Timber
 import javax.inject.Inject
 
 
-class GitRepoListFragment : DaggerFragment() {
+class GitRepoListFragment : DaggerFragment() , GitRepoListAdapter.Listener {
 
     @Inject
     lateinit var mViewModel:GitRepoListFeatureViewModel
 
+    private lateinit var linearLayoutManager: LinearLayoutManager
+
+    private val adapter: GitRepoListAdapter by lazy {
+        GitRepoListAdapter(this)
+    }
     override
     fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                      savedInstanceState: Bundle?): View? {
@@ -29,13 +38,26 @@ class GitRepoListFragment : DaggerFragment() {
         mViewModel.repoLiveData.observe(this, Observer<List<Repos>> { repos -> updateRepos(repos)})
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        linearLayoutManager = LinearLayoutManager(requireContext())
+        repo_items_recycler_view.layoutManager = linearLayoutManager
+        repo_items_recycler_view.adapter = adapter
+        repo_items_recycler_view.itemAnimator = DefaultItemAnimator().apply { supportsChangeAnimations = true }
+        ViewCompat.setNestedScrollingEnabled(repo_items_recycler_view,false)
+    }
     private fun updateRepos(repos: List<Repos>){
         Timber.d("Received ${repos.size} list of repos from repository  ")
+        adapter.setRepos(repos)
     }
 
     override fun onResume() {
         super.onResume()
         mViewModel.getRepos()
+    }
+
+    override fun onItemClick(repo: Repos) {
+
     }
 
     companion object {
